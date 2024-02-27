e0cfe3645d443dc4b9646d1cb93b772b6e5718b2
Nilo Soares <nilosoares@hotmail.com>
2024-02-26 22:42:53 -0300

    Add the Stack class (implements the StackInterface)

---
+++ src/Exception/EmptyStackException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CancioLabs\Ds\Stack\Exception;
+
+use Exception;
+use Throwable;
+
+class EmptyStackException extends Exception
+{
+
+    public function __construct(
+        string $message,
+        int $code = 0,
+        ?Throwable $previous = null
+    )
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
