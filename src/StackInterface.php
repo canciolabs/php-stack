906c2591156642eeec2b9d48b7d35e0a58c17ed7
Nilo Soares <nilosoares@hotmail.com>
2024-02-26 22:41:45 -0300

    Add docblocks to the Stack methods

--- src/StackInterface.php
+++ src/StackInterface.php
@@ -7,12 +7,24 @@ use Countable;
 interface StackInterface extends Countable
 {
 
-    public function push(mixed $value): static;
+    /**
+     * Add a new element to the top of the stack.
+     */
+    public function push(mixed $element): static;
 
+    /**
+     * Remove and return the top element.
+     */
     public function pop(): mixed;
 
+    /**
+     * Returns the top element of the stack.
+     */
     public function top(): mixed;
 
+    /**
+     * Test whether the stack is empty.
+     */
     public function isEmpty(): bool;
 
 }
